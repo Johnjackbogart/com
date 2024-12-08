83111e07d47effe2dabc5e95fc2b1f7aca1fab76
John Bogart <johnjackbogart@gmail.com>
2024-12-08 12:57:56 -0500

    adds scroll svg to scene

--- src/app/_components/three/playground.tsx
+++ src/app/_components/three/playground.tsx
@@ -3,7 +3,7 @@ import * as THREE from "three";
 import { useRef, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Physics } from "@react-three/rapier";
-import { MeshTransmissionMaterial } from "@react-three/drei";
+import { MeshTransmissionMaterial, Svg } from "@react-three/drei";
 import { easing } from "maath";
 
 import { Me } from "./avatar";
@@ -17,36 +17,33 @@ export default function PlayGround() {
 
   useEffect(() => {
     const handleWheel = (e: WheelEvent) => {
-      // Adjust this multiplier to control zoom speed
       scrollOffset.current += e.deltaY * 0.1;
     };
 
-    // Add a wheel event listener to window (or a scrollable container)
     window.addEventListener("wheel", handleWheel, { passive: true });
 
-    // Cleanup the event listener on unmount
     return () => {
       window.removeEventListener("wheel", handleWheel);
     };
   }, []);
   useFrame((state, delta) => {
     if (!tk.current) return;
+
     let cameraYOffset = state.pointer.y * 0.05 + scrollOffset.current - 100;
     const cameraZOffset = 5 + Math.cos(state.pointer.x) * 2;
     tk.current.rotation.z = 1 * state.clock.getElapsedTime();
     tk.current.rotation.x = Math.PI / 2;
+
     if (scrollOffset.current > 100 && scrollOffset.current < 200) {
       cameraYOffset = 0;
-    }
-    if (scrollOffset.current > 200 && scrollOffset.current < 400) {
+    } else if (scrollOffset.current > 200 && scrollOffset.current < 400) {
       console.log(scrollOffset.current);
       tk.current.rotation.x = (scrollOffset.current / 400) * Math.PI;
-      tk.current.position.z = scrollOffset.current / 50 - 2;
+      tk.current.position.z = scrollOffset.current / 50 - 4;
       cameraYOffset = 0;
-    }
-    if (scrollOffset.current > 400) {
+    } else if (scrollOffset.current > 400) {
       tk.current.rotation.x = Math.PI;
-      tk.current.position.z = scrollOffset.current / 50 - 2;
+      tk.current.position.z = scrollOffset.current / 50 - 4;
       cameraYOffset = 0;
     }
     easing.damp3(
@@ -55,11 +52,20 @@ export default function PlayGround() {
       0.1,
       delta,
     );
+    console.log(tk.current.position);
     state.camera.lookAt(0, 0, 0);
   });
 
   return (
     <Physics gravity={[0, 0, 0]}>
+      <Svg
+        src={"scroll.svg"}
+        position={[0, 0, 50]}
+        scale={0.1}
+        rotation={new THREE.Euler(Math.PI / 2, 0, 0)}
+        fillMaterial={{ color: "white" }}
+        strokeMaterial={{ color: "black" }}
+      />
       <spotLight position={[0, 0, 3]} penumbra={100} castShadow angle={0.2} />
       <ambientLight color="white" intensity={1} />
       <pointLight position={[0, 0, 3]} />
